9b021fa7e1db7945d07fd69b3100b52ad44a3403
Sebastian Miller <seb.miller1@gmail.com>
2017-12-24 17:35:17 +0900

    Created ble command with initialization command

--- commands/bluetooth.c
+++ commands/bluetooth.c
@@ -19,9 +19,68 @@
 #include "esp_gatts_api.h"
 
 
+static bool bluetooth_cmd_ble_is_inited = false;
+static bool bluetooth_cmd_controller_is_inited = false;
+static bool bluetooth_cmd_controller_is_enabled = false;
+static bool bluetooth_cmd_bluedroid_is_inited = false;
+static bool bluetooth_cmd_bluedroid_is_enabled = false;
+
 CLI_CMD(ble) {
     if ( CMD_HAS_ARG_AT(1, "on") ) {
-        cli_printf("BLE on\n");
+        esp_err_t ret;
+
+        if ( CMD_HAS_ARG("-h") ) {
+            cli_printf("Usage: %s on [OPTION]...\n", argv[0]);
+            cli_printf("initialize and start the bluetooth controller and stack.\n");
+            cli_printf("\n");
+            cli_printf("OPTION:\n");
+            cli_printf("  -h: show this help.\n");
+            return CLI_CMD_RETURN_OK;
+        }
+
+        if (bluetooth_cmd_ble_is_inited) {
+            cli_printf("BLE has already been turned on\n");
+            return CLI_CMD_RETURN_ERROR;
+        }
+
+        if ( !bluetooth_cmd_controller_is_inited ) {
+            esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+            ret = esp_bt_controller_init(&bt_cfg);
+            if (ret) {
+                cli_printf("BT controller initialize failed\n");
+                return CLI_CMD_RETURN_ERROR;
+            }
+            bluetooth_cmd_controller_is_inited = true;
+        }
+        if ( !bluetooth_cmd_controller_is_enabled ) {
+            ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+            if (ret) {
+                cli_printf("BT controller enable failed\n");
+                return CLI_CMD_RETURN_ERROR;
+            }
+            bluetooth_cmd_controller_is_enabled = true;
+        }
+        if ( !bluetooth_cmd_bluedroid_is_inited ) {
+            ret = esp_bluedroid_init();
+            if (ret) {
+                cli_printf("Bluedroid initialize failed\n");
+                return CLI_CMD_RETURN_ERROR;
+            }
+            bluetooth_cmd_bluedroid_is_inited = true;
+        }
+        if ( !bluetooth_cmd_bluedroid_is_enabled ) {
+            ret = esp_bluedroid_enable();
+            if (ret) {
+                cli_printf("Bluedroid enable failed\n");
+                return CLI_CMD_RETURN_ERROR;
+            }
+            bluetooth_cmd_bluedroid_is_enabled = true;
+        }
+
+        bluetooth_cmd_ble_is_inited = true;
+
+        cli_printf("BLE is now on\n");
+
         return CLI_CMD_RETURN_OK;
     }
 
