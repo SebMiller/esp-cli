0932988391bf318d8ded0ab30439d3a6352cb352
Sebastian Miller <seb.miller1@gmail.com>
2017-12-24 15:48:10 +0900

    Added Bluetooth commands file and config entry

---
+++ commands/bluetooth.c
@@ -0,0 +1,24 @@
+
+#include "sdkconfig.h"
+
+#if defined(CONFIG_CLI_USE_CMD_BLUETOOTH)
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "../cmd_create.h"
+#include "../cli.h"
+
+#include "esp_bt.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_gatt_common_api.h"
+#include "esp_gatts_api.h"
+
+
+
+
+#endif
